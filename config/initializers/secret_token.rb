579cfa7b3db1b2713ab86cd3ac41da3a9aca5919
John Stetter <john@slowplanet.net>
2012-08-17 00:14:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SongFinder::Application.config.secret_token = '9a0a5cdf171ae47e232fcfeebae3bb85a8db87709174e8f712d25499574f9ba87a04937f61101c1c51d8cdb90aafdd1b5b2589f0c762e64a426d394a51c6bccc'
